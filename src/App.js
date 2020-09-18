3ce20033221adcdf92e3b26697c2fed2af822661
RDMedicalBuy <71453531+RDMedicalBuy@users.noreply.github.com>
2020-09-18 15:10:37 -0500

    carousel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./styles.css";
+import Carousel from "react-elastic-carousel";
+import Card from "./Card";
 
-function App() {
+export default function App() {
+  const breakPoints = [
+    { width: 500, itemsToShow: 1 },
+    { width: 768, itemsToShow: 2 },
+    { width: 1200, itemsToShow: 3 },
+    { width: 1500, itemsToShow: 4 },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel breakPoints={breakPoints}>
+        <Card number="1" />
+        <Card number="2" />
+        <Card number="3" />
+        <Card number="4" />
+        <Card number="5" />
+        <Card number="6" />
+        <Card number="7" />
+        <Card number="8" />
+        <Card number="9" />
+        <Card number="10" />
+        <Card number="11" />
+        <Card number="12" />
+      </Carousel>
     </div>
   );
 }
-
-export default App;
